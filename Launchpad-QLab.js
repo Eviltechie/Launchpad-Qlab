1e7f84a9d4dc0a0907c25f322fedc58edb5833cf
Ivan Panarusky <panaruskyi@gmail.com>
2022-11-19 21:03:20 -0500

    can always be connected to a workspace

--- Launchpad-QLab.js
+++ Launchpad-QLab.js
@@ -31,16 +31,28 @@ function sendOSCInteger(address, integer) {
 
 //When we are connected...
 client.on("ready", function () {
-    sendOSCInteger("/updates", 1); //Subscribe to updates
     sendOSCAddress("/workspaces"); //List all workspaces. We will connect to the first one and ignore the rest.
 });
 
 var workspaceID;
 
+//Handle incoming /reply/workspaces and attempt to connect to first workspace
 function onWorkspaces(args) {
     args = JSON.parse(args[0]);
-    console.log("Using workspace " + args.data[0].displayName + " with ID " + args.data[0].uniqueID);
-    workspaceID = args.data[0].uniqueID
+    if (args.data.length == 0) {
+        console.log("No workspaces available, retrying in 2 seconds");
+        setTimeout(refreshWorkspaces, 2000);
+    } else {
+        console.log("Using workspace " + args.data[0].displayName + " with ID " + args.data[0].uniqueID);
+        workspaceID = args.data[0].uniqueID;
+        sendOSCAddress("/workspace/" + workspaceID + "/connect");
+        sendOSCInteger("/updates", 1);
+    }
+}
+
+//Request list of workspaces in order to attempt to connect to one in onWorkspaces
+function refreshWorkspaces() {
+    sendOSCAddress("/workspaces");
 }
 
 //Handle incoming OSC messages
@@ -52,6 +64,10 @@ client.on("message", function (oscMsg, timeTag, info) {
     switch (address) {
         case "/reply/workspaces":
             onWorkspaces(oscMsg.args);
+            break;
+        case "/update/workspace/" + workspaceID + "/disconnect":
+            console.log("Workspace shutting down");
+            refreshWorkspaces();
             break;
     }
 });
